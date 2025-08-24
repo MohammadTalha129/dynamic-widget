7eb3153e398917d9f559ddc1d9bcaacee6c891fe
MohammadTalha129 <mhmdtalha563@gmail.com>
2025-08-24 12:21:12 +0500

    Form critical css seprate

--- form-loader.js
+++ form-loader.js
@@ -8,7 +8,7 @@
   }
   Promise.all([
     fetch("https://dynamic-widget.pages.dev/form.min.html").then(r => r.text()),
-    fetch("https://dynamic-widget.pages.dev/form.min.css").then(r => r.text())
+    fetch("https://dynamic-widget.pages.dev/form-critical.min.css").then(r => r.text())
   ]).then(([html, css]) => {
     let styleTag = document.querySelector("style");
     if(!styleTag){
@@ -17,6 +17,10 @@
     }
     styleTag.textContent += "\n" + css;
     container.innerHTML = html;
+    let linkTag = document.createElement("link");
+    linkTag.rel = "stylesheet";
+    linkTag.href = "https://dynamic-widget.pages.dev/form.min.css";
+    document.head.appendChild(linkTag);
     const script = document.createElement("script");
     script.src = "https://dynamic-widget.pages.dev/form.min.js";
     script.defer = true;
